0286e23bf648273b4668efd8f0099d857290697f
Mehmet Seven <mehmetseven0@gmail.com>
2021-03-17 16:23:56 +0300

    mongo uri from env file

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose.connect(
-    'mongodb+srv://root:123456ff@cluster0.mlcqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    },
-  );
+  mongoose.connect(process.env.MONGODB_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
 
   mongoose.connection.on('open', () => {
     console.log('MongoDB: Connected');
